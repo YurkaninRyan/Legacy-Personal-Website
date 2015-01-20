f381f58ab3d2dd9ec41200af4c98cd23143ec35a
Ryan Yurkanin <yurkaninryan@hotmail.com>
2015-01-20 00:14:33 -0500

    finishing up splash page

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    nodemon = require('gulp-nodemon'),
+    livereload = require('gulp-livereload'),
+    util = require('gulp-util'),
+    uglify = require('gulp-uglify'),
+    minifyCSS = require('gulp-minify-css');
+
+gulp.task('less', function() {
+    // Builds the CSS
+    gulp.src(['public/less/styles.less'])
+        .pipe(less().on('error',util.log))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('public/css'));
+});
